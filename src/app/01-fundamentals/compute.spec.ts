7a0846c40aaaac2fc594f7d631e862cfee00b6e5
Thota, Siddartha <siddartha_thota@optum.com>
2018-09-15 14:22:21 -0400

    Updating tests

--- src/app/01-fundamentals/compute.spec.ts
+++ src/app/01-fundamentals/compute.spec.ts
@@ -1,13 +1,13 @@
 import {compute} from './compute';
 
 describe('compute', async() => {
-    it('should return 0 if input is negative'), function() {
+    it('should return 0 if input is negative', function() {
         const result = compute(-1);
         expect(result).toBe(0);
-    }
+    })
 
-    it('should return 2 if input is positive'), function() {
+    it('should return 2 if input is positive', function() {
         const result = compute(1);
         expect(result).toBe(2);
-    }
+    })
 })
